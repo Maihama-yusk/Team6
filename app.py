190a5c8ce43eb9b9a2669a4eab6cd196cca483fc
matsuo1234567 <122419523+matsuo1234567@users.noreply.github.com>
2023-02-26 11:29:50 +0900

    button text

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, abort
+from transformers import T5Tokenizer, GPT2LMHeadModel
+import torch
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,8 +10,64 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, FlexSendMessage, StickerSendMessage, TemplateSendMessage,ButtonsTemplate,MessageAction
 )
-from transformers import T5Tokenizer, GPT2LMHeadModel
-import torch
+
+from flask import Flask,render_template,request,abort
+
+line_bot_api = LineBotApi('qFv+ziwbtKuvF3+N6S+SleuNiydkE8jXLdYMxh3wUUQUXOTiSM12JlB3fktthpN+wlYiK5jCspKMK711hf5EZIvvYO5ewpYMz4cb3L8Kkj6/+helDE2SHnTiIerQV+Jclxs4s1YtB8qKUlbW4OVElQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('37ada14b83c43df19ce22f49deef23e8')
+
+def createbutton(text1,text2,text3):
+    payload = {
+        "type": "bubble",
+        "direction": "ltr",
+        "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+            "type": "text",
+            "align": "center",
+            "text": "どのテキストが適していますか？"
+            }
+        ]
+        },
+        "footer": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+            {
+            "type": "button",
+            "action": {
+                "type": "message",
+                "label": "1",
+                "text": "1"
+            },
+            "style": "primary"
+            },
+            {
+            "type": "button",
+            "action": {
+                "type": "message",
+                "label": "2",
+                "text": "2"
+            },
+            "style": "primary",
+            "margin": "5px"
+            },
+            {
+            "type": "button",
+            "action": {
+                "type": "message",
+                "label": "3",
+                "text": "3"
+            },
+            "margin": "5px",
+            "style": "primary"
+            }
+        ]
+        }
+    }
+    return payload
 
 def ml(input):
     tokenizer = T5Tokenizer.from_pretrained('rinna/japanese-gpt2-small')
@@ -34,74 +91,24 @@ def ml(input):
     for i, beam_output in enumerate(beam_outputs):
         s.append(tokenizer.decode(beam_output, skip_special_tokens=True))
     return s
+    
 
 
+#Flaskオブジェクトの生成
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('4W/LQ3IhGKdH5jobBYlYZbSvVz9f5P0g8FKQ4HQEvRfOeEUiAzC7goevo76pBdzAaCROTfsRUOVN3XGZoHQ8lHgHDIul1F/eHf8oUHkujU7iIfkyC9Hc+lKejBaM1QEsQ8wMIfunsvPmD5+GXW8t+AdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('4e5ed7673bcb498a1b456e1354684cb2')
 
-
-payload = {
-    "type": "bubble",
-    "direction": "ltr",
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "text",
-          "align": "center",
-          "text": "どのテキストが適していますか？"
-        }
-      ]
-    },
-    "footer": {
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        {
-          "type": "button",
-          "action": {
-            "type": "message",
-            "label": "1",
-            "text": "1"
-          },
-          "style": "primary"
-        },
-        {
-          "type": "button",
-          "action": {
-            "type": "message",
-            "label": "2",
-            "text": "2"
-          },
-          "style": "primary",
-          "margin": "5px"
-        },
-        {
-          "type": "button",
-          "action": {
-            "type": "message",
-            "label": "3",
-            "text": "3"
-          },
-          "margin": "5px",
-          "style": "primary"
-        }
-      ]
-    }
-  }
-
-flex_message = FlexSendMessage(
-  alt_text='this is alt_text',
-  contents=payload
-)
-
+#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
-def test():
-    a  = ml("あいうえお")
-    return a
+def hello():
+    ml("あいうえお")
+    return "ok"
+
+#「/index」へアクセスがあった場合に、「index.html」を返す
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -127,9 +134,9 @@ def callback():
 
 @handler.add(MessageEvent, message=(TextMessage))
 def handle_image_message(event):
+    reply_messages = []
     request_message = event.message.text
     reply_messages = []
-    button_reply_message = flex_message
     if request_message == '1':
         reply_messages = TextSendMessage(text='1が正しいと認識しました')
     elif request_message == '2':
@@ -137,16 +144,22 @@ def handle_image_message(event):
     elif request_message == '3':
         reply_messages = (TextSendMessage(text='3が正しいと認識しました'))
     else:
-        reply_messages.append(TextSendMessage(text='1の文章'))
-        reply_messages.append(TextSendMessage(text='2の文章'))
-        reply_messages.append(TextSendMessage(text='3の文章'))
-        button_reply_message = flex_message
-        reply_messages.append(button_reply_message)
+        a = ml(request_message)
+
+        reply_messages.append(TextSendMessage(text="1:"+str(a[0])))
+        reply_messages.append(TextSendMessage(text="2:"+str(a[1])))
+        reply_messages.append(TextSendMessage(text="3:"+str(a[2])))
+        button_reply_message = createbutton(str(a[0]),str(a[1]),str(a[2]))
+        flex_message = FlexSendMessage(
+            alt_text='this is alt_text',
+            contents=button_reply_message
+        )
+        reply_messages.append(flex_message)
     line_bot_api.reply_message(
         event.reply_token,
         reply_messages
     )
 
-
+#おまじない
 if __name__ == "__main__":
     app.run()
